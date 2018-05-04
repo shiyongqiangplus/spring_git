b8794af77fb3ae72a7172b43f0c5dd736fa82efd
syq <zuihou123yici@163.com>
2018-05-04 10:50:02 +0800

    提交我的第一个项目

---
+++ src/main/java/com/yss/spring/spring_git/SpringGitApplication.java
@@ -0,0 +1,12 @@
+package com.yss.spring.spring_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitApplication.class, args);
+	}
+}
